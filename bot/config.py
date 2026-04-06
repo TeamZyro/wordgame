7fc6f891a2a3ca0d41757fc5441d5683762b9aca
Tᴇᴀᴍ【ＺＹＲＯ】 <harshmanjhi0004@gmail.com>
2026-04-06 10:47:18 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -9,6 +9,5 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "8519924243:AAGwus55Q9rHdb8npYkshJDwnj2PkL_bx
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://harshmanjhi1801:webapp@cluster0.xxwc4.mongodb.net/?")
 DB_NAME = os.getenv("DB_NAME", "WordGridBotDB")
 SUDO_USERS = [int(x) for x in os.getenv("SUDO_USERS", "7638720582, 7638720582").split(",") if x.strip()]
-
-SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/ZyroSupport")
-UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "https://t.me/ZyroBots")
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/LoverCodesChat")
+UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "https://t.me/LoverCodes")
